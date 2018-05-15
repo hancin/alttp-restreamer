b686c9f6032ce6950326f9997b0b74cb06ac7e68
David Pratte <d.pratte@napec.ca>
2018-05-15 13:55:17 -0400

    More scrape code for reference.

--- extension/scrape_gamers.js
+++ extension/scrape_gamers.js
@@ -49,3 +49,34 @@
 
     return JSON.stringify(valid);
 })();
+
+(function runList(){
+    var entries = document.querySelectorAll("tr");
+    var valid = [];
+    var order = 0;
+
+    entries.forEach(line => {
+        //This is not restreamed by us.
+        if(line.cells[2].innerText.indexOf("ALTTPRandomizer") !== 0 && line.cells[2].innerText.indexOf("SpeedGaming") !== 0){
+            return;
+        }
+
+        var run = {
+            order: order,
+            name: line.cells[1].innerText,
+            time: line.cells[0].innerText,
+            runners: line.cells[1].innerText.split(" vs "),
+            channel: line.cells[2].innerText,
+            commentators: line.cells[3].innerText.replace('[SIGN UP]', '').trim().split(', '),
+            trackers: line.cells[4].innerText.replace('[SIGN UP]', '').trim().split(', '),
+            broadcasters: line.cells[2].innerText.startsWith('SpeedGaming') ? [] : line.cells[5].innerText.replace('[SIGN UP]', '').trim().split(', '),
+            pk: parseInt(line.cells[0].querySelector("a").id.substr(1))
+        };
+        
+        order += 1;
+
+        valid.push(run);
+    });
+
+    return JSON.stringify(valid);
+})();
